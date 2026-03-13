519ee265b0ff975cecb612fa3cc3cda64dc179da
Piyapat <6831503059@lamduan.mfu.ac.th>
2026-03-12 23:43:24 -0700

    feat: add local navbar

---
+++ app/components/LocalNavbar.tsx
@@ -0,0 +1,8 @@
+export default function LocalNavbar(){
+    return(
+        <div className="shadow-xl w-full h-16 flex items-center justify-between px-4">
+            <div className="font-bold text-xl">LifeMarkers</div>
+         
+        </div>
+    );
+}
