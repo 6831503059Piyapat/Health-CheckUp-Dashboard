8dfb1ebf482883d9edbbc393586244a34cc1c29c
Piyapat <6831503059@lamduan.mfu.ac.th>
2026-03-30 21:12:55 +0700

    Fix : Scroll Lab result

--- app/components/Result.tsx
+++ app/components/Result.tsx
@@ -36,9 +36,10 @@ export default function Result({ui}:Props) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
-    <div className="max-w-3xl mx-auto p-10 bg-white w-[100vh] rounded-xl shadow-sm border border-gray-100">
+    <div className="max-w-3xl h-[80vh] mx-auto p-10 bg-white w-[100vh]  rounded-xl shadow-sm">
       {/* Header */}
-      <div className="flex justify-between items-center mb-2">
+      <div className='rounded-xl p-4 mb-4 h-[70vh]'>
+      <div className="flex justify-between items-center mb-4   ">
         <h2 className="text-2xl font-bold text-slate-800">Lab Result</h2>
         <button onClick={()=>ui(false)} className="flex items-center text-gray-500 hover:text-red-500 cursor-pointer transition-colors">
           <span  className="mr-1 text-sm">close</span>
@@ -48,11 +49,11 @@ export default function Result({ui}:Props) {
       
 
       {/* List Items */}
-      <div className="space-y-3">
+      <div className="space-y-3 h-[60vh] overflow-y-auto [ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:display-none">
         {dataFetch.map((value,i)=>(
             <div 
               key={i}
-              className="group flex items-center justify-between p-4 bg-gray-100 rounded-xl cursor-pointer hover:bg-gray-200 transition-all"
+              className="group  flex items-center justify-between p-4 bg-gray-100 rounded-xl cursor-pointer hover:bg-gray-200 transition-all"
             >
               <h1 className='relative w-full'>{value.dateFile}</h1>
               <span className="text-slate-600 font-medium">{value.suggest}</span>
@@ -62,7 +63,7 @@ export default function Result({ui}:Props) {
         ))}
      
       </div>
-
+</div>
       {/* Footer Action */}
       
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ return(
     <div className="flex min-h-screen bg-slate-50 font-sans text-slate-900">
  {/* <HealthcareRec/> */}
       {/* SIDEBAR */}
-
+  
       {resulUi &&(<Result ui={setResultUi}/>)}
       
       <Navbar />
